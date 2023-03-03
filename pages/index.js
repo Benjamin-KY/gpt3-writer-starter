4891714c8525aa109a852448b49f105078a014b3
Benjamin Kereopa-Yorke <99622824+Benjamin-KY@users.noreply.github.com>
2023-03-03 14:16:13 +1000

    Update index.js
    Customised the code from BuildSpace. Wrote code for textinput, useState etc

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,37 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };  
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>GPT-3 Writer App Build with a React/NextJS/CSS Front-end</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ben Kereopa-Yorke</h2>
           </div>
         </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText}/>
+        </div>
+        <div className="prompt-buttons">
+           <a className="generate-button" onClick={null}>
+              <div className="generate">
+                  <p>Generate</p>
+            </div>
+          </a>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
-};
+  </div>
+  </div>
+    );
+  };
 
 export default Home;
